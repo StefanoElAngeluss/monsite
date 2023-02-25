a6e1f6f76d31a364c971dea70c3017519cad9dc2
StephaneD <stephanedavy@outlook.fr>
2023-02-25 17:45:48 +0100

    Articles en place FIN

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  validates :titre, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :contenu, presence: true, length: { minimum: 10, maximum: 1000 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'pages#accueil'
   get 'pages/boutique', to: 'pages#boutique', as: :boutique
   get 'pages/contact', to: 'pages#contact', as: :contact
