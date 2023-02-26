d11f134e7594d444c8533b1d0c8ce14119f850e5
StephaneD <stephanedavy@outlook.fr>
2023-02-26 08:13:11 +0100

    ajout de views pour user et article + devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
-  validates :titre, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :contenu, presence: true, length: { minimum: 10, maximum: 1000 }
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :username,
+      :email,
+      :rue,
+      :ville,
+      :code_postale,
+      :pays
+    ])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,37 @@
 Rails.application.routes.draw do
-  resources :articles
+  ########## USERS ##########
+  devise_for :users
+  get "profile/:id", to: "users#profile", as: "profile"
+
+  ########## ADMINISTRATION ##########
+  # authenticated :user, ->(user) { user.administrateur? } do
+  # get "admin", to: "admin#index"
+  # get "admin/articles"
+  # get "admin/comments"
+  # get "admin/users"
+  # get "admin/show_post/:id", to: "admin#show_post", as: "admin_article"
+  # end
+
+  ########## PAGES ##########
   root 'pages#accueil'
   get 'pages/boutique', to: 'pages#boutique', as: :boutique
   get 'pages/contact', to: 'pages#contact', as: :contact
   get 'pages/about', to: 'pages#about', as: :about
+
+  ########## STRIPE & PAY ##########
+  # get "checkout", to: "checkouts#show"
+  # get "checkout/success", to: "checkouts#success"
+  # get "billing", to: "billing#show"
+  # get 'members/dashboard'
+
+  ########## ARTICLES ##########
+  resources :articles
+  # resources :articles do
+  #   resources :comments
+  # end
+  # Follow path
+  # post "follow/:id", to: "follows#follow", as: :follow
+  # Unfollow path
+  # post "unfollow/:id", to: "follows#unfollow", as: :unfollow
+  resources :categories
 end
